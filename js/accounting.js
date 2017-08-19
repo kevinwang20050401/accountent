0a26639930b7c4e87012fd54625c3375e167e4b1
kevinwang20050401 <kevin20050401@gmail.com>
2017-08-19 20:30:09 +0800

    account

---
+++ js/accounting.js
@@ -0,0 +1,22 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("mydb");
+var accountingCollection = db.collection("students");
+accountingCollection.load();
+$("#sub").click(function(){
+	var date=$("#date").val();
+	var kind=$("#kind").val();
+	var item=$("#item").val();
+	var cost=$("#cost").val();
+accountingCollection.insert({
+    date:date,
+    kind:kind,
+    item:item,
+    cost:cost
+});
+accountingCollection.save();
+alert("儲存成功");
+var date=$("#date").val("");
+var kind=$("#kind").val("");
+var item=$("#item").val("");
+var cost=$("#cost").val("");
+});
